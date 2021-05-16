a6f1db1b3c90792cdb7aa7f733c373a64a785031
rsouth <southy98@gmail.com>
2021-05-16 11:21:16 +0100

    .gitignore and hello_world.asm formatting

--- code/hello_world.asm
+++ code/hello_world.asm
@@ -40,21 +40,21 @@ section .text
         int 0x80
         ret
 
-        ; == is_eq function =========================
+    ; == is_eq function =========================
     _is_eq:
         mov edx,is_equal_len
         mov ecx,is_equal
         call _print
         jmp _exit
 
-        ; == is_not_eq function =====================
+    ; == is_not_eq function =====================
     _is_not_eq:
         mov edx,is_not_equal_len
         mov ecx,is_not_equal
         call _print
         jmp _exit
 
-        ; == _clear_buffer function =================
+    ; == _clear_buffer =================
     _clear_buffer:
         mov edx, 1
         mov ecx, name
@@ -65,49 +65,49 @@ section .text
         jne _clear_buffer
         ret
 
-        ; == _exit function =========================
+    ; == _exit =========================
     _exit:
         mov eax,1
         int 0x80
 
-        ; == _start function - entry point ==========
+    ; == _start - entry point ==========
     _start:
-        ; write 'Hello World' to stdout
-        mov ebx,1             ; 1 is the file descriptor for stdout
-        mov eax,4             ; 4 is the syscall for sys_write
-        mov	edx,len           ; length of 'Hello World'
-        mov	ecx,msg           ; null-terminated 'Hello World'
-        int 0x80              ; syscall
+                                ; write 'Hello World' to stdout
+        mov ebx,1               ; 1 is the file descriptor for stdout
+        mov eax,4               ; 4 is the syscall for sys_write
+        mov	edx,len             ; length of 'Hello World'
+        mov	ecx,msg             ; null-terminated 'Hello World'
+        int 0x80                ; syscall
 
-                              ; prompt for user input
-                              ; note the syscall has moved to _print function
+                                ; prompt for user input
+                                ; note the syscall has moved to _print function
         mov edx,asklen
         mov ecx,ask
-        call _print           ; call returns execution
+        call _print             ; call returns execution
 
-                              ; take user input from stdin
-        mov ecx,name          ; buffer
-        mov ebx,0             ; 0 is the file descriptor for stdin
-        mov eax,3             ; 3 is the syscall for sys_read
+                                ; take user input from stdin
+        mov ecx,name            ; buffer
+        mov ebx,0               ; 0 is the file descriptor for stdin
+        mov eax,3               ; 3 is the syscall for sys_read
         int 0x80
 
-        mov edx,eax           ; eax contained the size of the input
-                              ; check for LF byte
+        mov edx,eax             ; eax contained the size of the input
+                                ; check for LF byte
         cmp byte [ecx + eax - 1], 10
-        ; jump if the zero flag is set (==1) which means that
-        ; the cmp was 'not equal'. Otherwise flow continues.
+                                ; jump if the zero flag is set (==1) which means
+                                ; the cmp was 'not equal'. Otherwise continues.
         jz do_maffs
         call _clear_buffer
 
 do_maffs:
-        ; addition example
-        mov edx,1             ; put 1 into edx
-        mov ecx,1             ; put 1 into ecx
-        add ecx,edx           ; ecx = ecx + edx
-        cmp ecx,2             ; if ecx == 2
-        je _is_eq             ; if ecx == 2
+                                ; addition example
+        mov edx,1               ; put 1 into edx
+        mov ecx,1               ; put 1 into ecx
+        add ecx,edx             ; ecx = ecx + edx
+        cmp ecx,2               ; if ecx == 2
+        je _is_eq               ; if ecx == 2
 
-                              ; quit the program
-        mov	eax,1             ; system call for sys_exit
+                                ; quit the program
+        mov	eax,1               ; system call for sys_exit
         int	0x80
 
